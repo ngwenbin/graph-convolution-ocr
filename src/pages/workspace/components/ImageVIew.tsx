574e8b1ea715d088caec730fac28bd6d2e657a5b
Wen Bin <nwb@super-powerful-fruit.local>
2024-09-16 09:49:54 +0800

    task: changed to a better pinch zoom package

--- src/pages/workspace/components/ImageVIew.tsx
+++ src/pages/workspace/components/ImageVIew.tsx
@@ -1,113 +1,55 @@
-import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 import { ImageFile } from "./file-upload/FileDisplay";
 import { Button } from "@/components/core/button";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useRef } from "react";
+import PinchZoom from "react-quick-pinch-zoom";
+import QuickPinchZoom, {
+  make3dTransformValue,
+  UpdateAction,
+} from "react-quick-pinch-zoom";
 
 interface ImageViewProps {
   file: ImageFile;
 }
 
 export function ImageView({ file }: ImageViewProps) {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const [loading, setLoading] = useState(true);
-  const [containerWidth, setContainerWidth] = useState<number>(0);
-  const [containerHeight, setContainerHeight] = useState<number>(0);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const containerRef = useRef<PinchZoom>(null);
 
-  const [imageNaturalWidth, setImageNaturalWidth] = useState<number>(0);
-  const [imageNaturalHeight, setImageNaturalHeight] = useState<number>(0);
+  const onUpdate = useCallback(({ x, y, scale }: UpdateAction) => {
+    if (imgRef.current) {
+      const value = make3dTransformValue({ x, y, scale });
 
-  const imageScale = useMemo(() => {
-    if (
-      containerWidth === 0 ||
-      containerHeight === 0 ||
-      imageNaturalWidth === 0 ||
-      imageNaturalHeight === 0
-    )
-      return 0;
-    const scale = Math.min(
-      containerWidth / imageNaturalWidth,
-      containerHeight / imageNaturalHeight
-    );
-    return scale;
-  }, [containerWidth, containerHeight, imageNaturalWidth, imageNaturalHeight]);
-
-  const handleResize = useCallback(() => {
-    if (containerRef.current) {
-      const rect = containerRef.current.getBoundingClientRect();
-      setContainerWidth(rect.width);
-      setContainerHeight(rect.height);
-    } else {
-      setContainerWidth(0);
-      setContainerHeight(0);
+      imgRef.current.style.setProperty("transform", value);
     }
   }, []);
 
-  useEffect(() => {
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [handleResize]);
-
-  const handleImageOnLoad = (image: HTMLImageElement) => {
-    setImageNaturalWidth(image.naturalWidth);
-    setImageNaturalHeight(image.naturalHeight);
+  const reset = () => {
+    if (containerRef.current) {
+      containerRef.current.scaleTo({ x: 0, y: 0, scale: 1, animated: false });
+    }
   };
 
   useEffect(() => {
-    setLoading(true);
-    const image = new Image();
-    image.onload = async () => {
-      handleImageOnLoad(image);
-      await new Promise((res) => {
-        setTimeout(res, 500);
-      });
-      setLoading(false);
-    };
-    image.src = file.src;
-  }, [file.src]);
+    reset();
+  }, [file.id]);
 
   return (
-    <div className="w-full h-full bg-gray-100" ref={containerRef}>
-      {!loading && imageScale > 0 && (
-        <TransformWrapper
-          key={`${containerWidth}x${containerHeight}`}
-          initialScale={imageScale}
-          minScale={imageScale * 0.8}
-          centerOnInit
-          pinch={{
-            disabled: true,
-          }}
-          doubleClick={{
-            mode: "reset",
-          }}
-        >
-          {({ resetTransform }) => (
-            <>
-              <Button
-                variant="outline"
-                className="absolute top-1 right-1 z-10"
-                onClick={() => resetTransform()}
-              >
-                Reset
-              </Button>
-              <TransformComponent
-                wrapperStyle={{
-                  width: "100%",
-                  height: "100%",
-                }}
-              >
-                <img
-                  src={file.src}
-                  alt="selected-image"
-                  onLoad={() => resetTransform()}
-                />
-              </TransformComponent>
-            </>
-          )}
-        </TransformWrapper>
-      )}
-    </div>
+    <>
+      <Button
+        className="absolute z-10 top-1 right-1"
+        variant="outline"
+        onClick={reset}
+      >
+        Reset
+      </Button>
+      <QuickPinchZoom centerContained onUpdate={onUpdate} ref={containerRef}>
+        <img
+          ref={imgRef}
+          className="w-full h-full "
+          src={file.src}
+          alt="selected-image"
+        />
+      </QuickPinchZoom>
+    </>
   );
 }
